470e9a290f4e7544ed232aba859894eb07cdac5b
Marc-André Bélanger <marc-andre.belanger@asciimail.com>
2021-12-05 13:55:39 -0500

    Fix deploy script.

--- client/deploy.sh
+++ client/deploy.sh
@@ -3,23 +3,15 @@
 now=$(date +"%m_%d_%Y")
 base="/usr/local/var/websites"
 forensics="${base}/forensics.gambitscheme.org"
-git_url="https://github.com/udem-dlteam/forensics.git"
 
-if [ -d "forensics" ]
-then
-    (cd forensics; git pull)
-else
-    git clone "$git_url"
-fi
-
-echo $PWD
+(cd ..; git pull)
 
 # NOTE: For now, we keep the UI in the v2 directory
 if [ -d "_deploy" ]
 then
-    mv  "_deploy_$now"
+    mv  _deploy "_deploy_$now"
 fi
 
-cp -R ./forensics/client/v2 _deploy
+cp -r ./v2 _deploy
 
-sudo cp -R _deploy/ "${forensics}/"
+sudo rsync -a ./_deploy/ "${forensics}/"
